bb62160d5bd55226ee2337723896aa4d450b0e57
DionHermans <64582304+DionHermans@users.noreply.github.com>
2022-10-22 09:59:10 +0200

    Converted basic prediction according to lab1

---
+++ prediction-api/bodyfat_predictor.py
@@ -0,0 +1,39 @@
+import json
+
+import pandas as pd
+from flask import jsonify
+import pickle
+
+
+class BodyFatPredictor:
+    def __init__(self):
+        self.model = None
+        self.trans = None
+
+    def predict_single_record(self, prediction_input):
+        print(prediction_input)
+        if self.model is None:
+            self.model = pickle.load(open('model.pkl', 'rb'))
+        print(json.dumps(prediction_input))
+        df = pd.read_json(json.dumps(prediction_input), orient='records')
+        print(df)
+
+        # Pre-calculations made for the prediction
+        df['Bmi'] = 703 * df['Weight'] / (df['Height'] * df['Height'])
+        df['ACratio'] = df['Abdomen'] / df['Chest']
+        df['HTratio'] = df['Hip'] / df['Thigh']
+        df.drop(['Weight', 'Height', 'Abdomen', 'Chest', 'Hip', 'Thigh'], axis=1, inplace=True)
+
+        # Transform to create a uniform dataset
+        if self.trans is None:
+            self.trans = pickle.load(open('transformer.pkl', 'rb'))
+
+        df_t = self.trans.transform(df)
+
+        # Prediction step
+        y_pred = self.model.predict(df_t)
+        print(y_pred)
+        status = (y_pred > 0.5)
+        print(type(status))
+        # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
+        return jsonify({'result': str(status[0])}), 200
---
+++ prediction-ui/app.py
@@ -0,0 +1,45 @@
+# importing Flask and other modules
+import json
+
+import requests
+from flask import Flask, request, render_template
+
+# Flask constructor
+app = Flask(__name__)
+
+
+# A decorator used to tell the application
+# which URL is associated function
+@app.route('/checkbodyfat', methods=["GET", "POST"])
+def check_diabetes():
+    if request.method == "POST":
+        prediction_input = [
+            {
+                "Age": int(request.form.get("Age")),  # getting input with name = ntp in HTML form
+                "Neck": int(request.form.get("Neck")),  # getting input with name = pgc in HTML form
+                "Knee": int(request.form.get("Knee")),
+                "Ankle": int(request.form.get("Ankle")),
+                "Biceps": int(request.form.get("Biceps")),
+                "Forearm": float(request.form.get("Forearm")),
+                "Wrist": float(request.form.get("Wrist")),
+                "Weight": float(request.form.get("Weight")),
+                "Height": float(request.form.get("Height")),
+                "Abdomen": float(request.form.get("Abdomen")),
+                "Chest": float(request.form.get("Chest")),
+                "Hip": float(request.form.get("Hip")),
+                "Thigh": int(request.form.get("Thigh"))
+            }
+        ]
+        print(prediction_input)
+        # use requests library to execute the prediction service API by sending a HTTP POST request
+        # localhost or 127.0.0.1 is used when the applications are on the same machine.
+        res = requests.post('http://localhost:5000/bodyfat_predictor/', json=json.loads(json.dumps(prediction_input)))
+        print(res.status_code)
+        result = res.json()
+        return result
+    return render_template(
+        "user_form.html")  # this method is called of HTTP method is GET, e.g., when browsing the link
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
